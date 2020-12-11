613f94240840a0d46168b9c86131707500197cde
João Correia <46303606+jpcorreia99@users.noreply.github.com>
2020-12-11 16:09:45 +0000

    Adicionar qr code reader (#2)

--- Terceira Fase/ArmazemInteligente/src/main/java/Model/Armazem/Stock/Palete.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/Model/Armazem/Stock/Palete.java
@@ -1,4 +1,25 @@
 package Model.Armazem.Stock;
 
 public class Palete {
+    private static int numeroPaletes = 0;
+
+    private int id;
+    private String material;
+
+    public static void atualizaNumeroPaletes(int n) {
+        numeroPaletes = n;
+    }
+    // construtor de uma nova palete
+    public Palete(String material) {
+        numeroPaletes++;
+        this.id = numeroPaletes;
+        this.material = material;
+    }
+
+    // construtor quando consultado da BD
+    public Palete(int id, String material) {
+        this.id = id;
+        this.material = material;
+    }
+
 }
---
+++ Terceira Fase/ArmazemInteligente/src/main/java/Util/RequestSender.java
@@ -0,0 +1,57 @@
+package Util;
+
+import com.google.zxing.WriterException;
+
+import java.io.*;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+
+public class RequestSender {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Material a codificar em código QR: ");
+        // Material dado como input
+        String material = scanner.nextLine();
+
+        // Path onde o código QR será guardado, terá uma timestamp
+        ZonedDateTime date =  ZonedDateTime.now();
+        String timestamp = DateTimeFormatter.ofPattern("dd_MM_yyyy__hh_mm_ss").format(date);
+        String path = "src/main/resources/"+ timestamp.toString()+".png";
+
+        // Encoding charset
+        String charset = "UTF-8";
+
+        // Cria o código QR
+        // guarda-lo no path indicado em cima
+        try {
+            criarQR(material, path, charset, 50, 50);
+            System.out.println("Código QR gerado com sucesso");
+        } catch (IOException | WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void criarQR(String data, String path,
+                               String charset,
+                               int height, int width)
+            throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(
+                new String(data.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, width, height);
+
+        MatrixToImageWriter.writeToFile(
+                matrix,
+                path.substring(path.lastIndexOf('.') + 1), // indica o formato (.png)
+                new File(path));
+    }
+}
