72820d65e781b33956947bbf8aaec261c336f4df
Elstryker <rubencorreiacerqueira@gmail.com>
2020-12-12 17:55:31 +0000

    Implemented Prateleira and PrateleiraDAO

--- Terceira Fase/ArmazemInteligente/src/main/java/Model/Armazem/Stock/PrateleiraDAO.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/Model/Armazem/Stock/PrateleiraDAO.java
@@ -6,97 +6,280 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 
 public class PrateleiraDAO implements Map<Integer,Prateleira> {
+    /**
+     * Contrutor por defeito que cria uma tabela nova na Base de Dados se esta não existir
+     */
     public PrateleiraDAO() {
-        Connection conn = DBConnect.connect();
-        try {
+        try (Connection conn = DBConnect.connect()) {
             Statement sta = conn.createStatement();
             String sql = "CREATE TABLE IF NOT EXISTS Prateleira (\n" +
                     "  id INT NOT NULL PRIMARY KEY,\n" +
-                    "  estaOcupado TINYINT NOT NULL);";
+                    "  estaOcupada TINYINT NOT NULL);";
             sta.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            DBConnect.close(conn);
         }
     }
 
+    /**
+     * Calcula o tamanho da estrutura de dados
+     * @return Tamanho
+     */
     @Override
     public int size() {
         int i = 0;
-        Connection conn = DBConnect.connect();
-        try {
+        try (Connection conn = DBConnect.connect()) {
             Statement sta = conn.createStatement();
-            String sql = "SELECT count(*) AS Total from Prateleira";
+            String sql = "SELECT count(*) AS Total FROM Prateleira";
             ResultSet rs = sta.executeQuery(sql);
             if(rs.next())
                 i = rs.getInt("Total");
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            DBConnect.close(conn);
         }
         return i;
     }
 
+    /**
+     * Verifica se a estrutura de dados se encontra vazia
+     * @return True se estiver, false caso contrário
+     */
     @Override
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    /**
+     * Verifica se a estrutura de dados contém uma Chave específica
+     * @param key Chave a procurar
+     * @return True se existir, false caso contrario
+     */
     @Override
     public boolean containsKey(Object key) {
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            String sql = "SELECT * FROM Prateleira" +
+                    " WHERE id = " + key.toString();
+            ResultSet rs = sta.executeQuery(sql);
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
+    /**
+     * Verifica se a estrutura de dados contém um valor específico
+     * @param value Valor a procurar
+     * @return True se existir, false caso contrário
+     */
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        Prateleira p = (Prateleira) value;
+        return containsKey(p.getId());
     }
 
+    /**
+     * Obtém o valor da base de dados associado a uma dada chave
+     * @param key Chave a procurar
+     * @return O respetivo valor
+     */
     @Override
     public Prateleira get(Object key) {
-
-        return null;
+        Prateleira p = null;
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            String sql = "SELECT * FROM Prateleira" +
+                    " WHERE id = " + key.toString();
+            int idPalete = getIdPalete(key.toString(),sta);
+            ResultSet rs = sta.executeQuery(sql);
+            if(rs.next())
+                p = new Prateleira(rs.getInt(1),rs.getBoolean(2), idPalete);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return p;
     }
 
+    /**
+     * Retorna o id da Palete contida na dada Prateleira
+     * @param key Id da Prateleira
+     * @param sta Canal que comunica com a BD
+     * @return id da Palete
+     */
+    private int getIdPalete(String key, Statement sta) {
+        int res = 0;
+        try {
+            ResultSet rs = sta.executeQuery("SELECT * FROM Palete" +
+                    " WHERE Prateleira =" + key);
+            if(rs.next())
+                res = rs.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     * Acrescenta uma nova prateleira na base de dados
+     * @param key id da prateleira
+     * @param value prateleira
+     * @return null se não houver chave na base de dados, ou a prateleira que anteriormente teria essa chave
+     */
     @Override
     public Prateleira put(Integer key, Prateleira value) {
-        return null;
+        Prateleira p = null;
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            if(containsKey(key))
+                p = get(key);
+            String sql = "INSERT INTO Prateleira VALUES (" +
+                    value.getId() + "," +
+                    value.isEstaOcupada() + ")" +
+                    "ON DUPLICATE KEY UPDATE estaOcupada = VALUES(estaOcupada);";
+            sta.executeUpdate(sql);
+            if(value.isEstaOcupada()) {
+                sql = "INSERT INTO Palete VALUES (" +
+                        value.getIdPalete() + "," +
+                        value.getId() + "," +
+                    "ON DUPLICATE KEY UPDATE prateleira = VALUES(prateleira);";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return p;
     }
 
+    /**
+     * Remove uma prateleira da BD
+     * @param key Chave a remover
+     * @return Prateleira removida
+     */
     @Override
     public Prateleira remove(Object key) {
-        return null;
+        Prateleira p = get(key);
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            if(p.isEstaOcupada())
+                sta.executeUpdate("UDPATE Palete SET prateleira = null WHERE id =" + p.getIdPalete()+ ";");
+            sta.executeUpdate("DELETE FROM Prateleira WHERE id = "+ p.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return p;
     }
 
+    /**
+     * Insere todos os elementos do Mapa dado como argumento na BD
+     * @param m Estrutura com os dados a inserir
+     */
     @Override
     public void putAll(Map<? extends Integer, ? extends Prateleira> m) {
-
+        for(Prateleira p: m.values())
+            put(p.getId(),p);
     }
 
+    /**
+     * Limpa a BD de todos os registos.
+     */
     @Override
     public void clear() {
-
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            sta.executeUpdate("UPDATE Palete SET prateleira = null;");
+            sta.executeUpdate("DELETE FROM Prateleira");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Apresenta um agrupamento de Chaves que pode ser percorrido
+     * @return Agrupamento com todas as chaves das prateleiras
+     */
     @Override
     public Set<Integer> keySet() {
-        return null;
+        Set<Integer> set = new HashSet<>();
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            ResultSet rs = sta.executeQuery("SELECT id FROM Prateleira");
+            while(rs.next())
+                set.add(rs.getInt(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return set;
     }
 
+    /**
+     * Devolve uma coleção com todos as Prateleira existentes na Base de Dados
+     * @return Coleção de todas as prateleiras
+     */
     @Override
     public Collection<Prateleira> values() {
-        return null;
+        Collection<Prateleira> col = new HashSet<>();
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            ResultSet rs = sta.executeQuery("SELECT id FROM Prateleira");
+            while(rs.next())
+                col.add(this.get(rs.getInt(1)));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return col;
     }
 
+    /**
+     * Devolve uma agrupamento com todos os pares id->Prateleira existentes na Base de Dados
+     * @return Coleção com toda a informação presente
+     */
     @Override
     public Set<Entry<Integer, Prateleira>> entrySet() {
-        return null;
+        Set<Entry<Integer,Prateleira>> res = new HashSet<>();
+        Entry<Integer,Prateleira> temp;
+        int id;
+        try (Connection conn = DBConnect.connect()) {
+            Statement sta = conn.createStatement();
+            ResultSet rs = sta.executeQuery("SELECT id FROM Prateleira");
+            while(rs.next()) {
+                id = rs.getInt(1);
+                temp = new MyEntry<>(id,this.get(id));
+                res.add(temp);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    final class MyEntry<K, V> implements Map.Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public MyEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V old = this.value;
+            this.value = value;
+            return old;
+        }
     }
 }
