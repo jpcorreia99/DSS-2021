ff6314da5deac5932f91ab51cacee29253ca4b81
Elstryker <rubencorreiacerqueira@gmail.com>
2020-12-23 11:18:36 +0000

    Fuck thos shot

--- Terceira Fase/ArmazemInteligente/src/main/java/Business/Armazem/IRobo.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/Business/Armazem/IRobo.java
@@ -1,4 +1,17 @@
 package Business.Armazem;
 
+import Util.Coordenadas;
+import Util.EstadoRobo;
+import Util.ResultadosMovimentoRobos;
+import Util.Tuple;
+
+import java.util.List;
+
 public interface IRobo {
+    boolean existemRobosDisponiveis();
+    Tuple<Integer,Coordenadas> encontraRoboLivre(int idPalete);
+    void transmiteInfoRota(int idPalete, int idPrateleira , int idRobo, List<Coordenadas> percurso, EstadoRobo estadoATer);
+    ResultadosMovimentoRobos recolheNotificacoes();
+    Coordenadas getCoordPalete(int idPalete);
+    List<Tuple<Coordenadas, Boolean>> getInfoRobos();
 }
--- Terceira Fase/ArmazemInteligente/src/main/java/Business/Armazem/IStock.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/Business/Armazem/IStock.java
@@ -1,5 +1,20 @@
 package Business.Armazem;
 
+import Util.EstadoPalete;
+import Util.Tuple;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IStock {
-    
+    boolean armazemTemEspacoDisponivel();
+    boolean existemPaletesRecemChegadas();
+    int getPaleteRecemChegada();
+    void marcaPaleteEmLevantamento(int idPalete);
+    void assinalaPaleteEmTransporte(int idPalete);
+    void assinalaPaletesArmazenadas(List<Tuple<Integer,Integer>> tuplosPaletesArmazenadasPrateleiras);
+    int encontraPrateleiraLivre();
+    Map<Integer,Tuple<String, EstadoPalete>> getLocPaletes();
+    int getIdPrateleiraGuardaPalete(int idPalete);
+    List<Integer> getPrateleirasOcupadas();
 }
