ef192dc38aefe024cc5e94b29a1811741f1cdfd4
jpcorreia99 <jpcorreia99@gmail.com>
2020-12-23 12:38:06 +0000

    utilizar 3 robos

--- Terceira Fase/ArmazemInteligente/src/main/java/APP.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/APP.java
@@ -29,7 +29,7 @@ public class APP {
             Thread[] threadsRobosExternos = new Thread[4];
             RoboTransportador[] robosExternos = new RoboTransportador[4];
             RoboTransportadorDAO roboTransportadorDAO = RoboTransportadorDAO.getInstance();
-            for (int i = 0; i <= 3; i++) {
+            for (int i = 0; i <= 2; i++) {
                 RoboTransportador robo = roboTransportadorDAO.getRoboTransportador(i+1);
                 robosExternos[i] = robo;
             }
@@ -51,7 +51,7 @@ public class APP {
             ui.show("Leitor de códigos códigos QR desligado!");
 
             // termina cada um dos robos
-            for (int i = 0; i<=3; i++){
+            for (int i = 0; i<=2; i++){
                 robosExternos[i].desliga();
                 try {
                     threadsRobosExternos[i].join();
--- Terceira Fase/ArmazemInteligente/src/main/java/Business/IArmazemLN.java
+++ Terceira Fase/ArmazemInteligente/src/main/java/Business/IArmazemLN.java
@@ -8,8 +8,18 @@ import java.util.Map;
 
 public interface IArmazemLN {
     void start();
+
+    /**
+     * Devolve um conjunto de informação relativa a cada palete existente no sistema
+     * @return Map em que a chave é o id de uma palete e o valor é uma estrutura composta por:
+     * tuplo composto pelo material da palete e um segundo tuplo
+     * composto pelo estado da palete e as suas coordenadas
+     */
     Map<Integer,Tuple<String, Tuple<EstadoPalete, Coordenadas>>> getPaletes();
+
     boolean login (String user, String password);
+
     int[][] getMapa ();
+
     void desligaSistema();
 }
